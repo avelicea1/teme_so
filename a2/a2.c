32ebc7ee6353c92b00201ebe38763f25c19b1aa6
Andreea <avelicea1@yahoo.com>
2023-04-26 15:38:02 +0300

    second task

--- a2/a2.c
+++ a2/a2.c
@@ -3,8 +3,30 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include "a2_helper.h"
-pid_t pid1=0,pid2=0,pid3=0,pid4=0,pid5=0,pid6=0,pid7=0;
+#include <pthread.h>
+#include <semaphore.h>
+typedef struct{
+    int value;
+}TH_STRUCT;
+typedef struct{
+    int id;
+    sem_t *mutex;
+    sem_t *barrier;
+}TH_STRUCT_P5;
+void *thread_function(void *args){
+    TH_STRUCT *s = (TH_STRUCT*)args;
+    info(BEGIN,7,s->value);
+    if(s->value ==4){ 
+        pthread_t tid1;
+        int i=1;
+        pthread_create(&tid1,NULL,thread_function,&i);
+        pthread_join(tid1,NULL);
+    }
+    info(END,7,s->value);
+    return NULL;
+}
 int main(){
+    pid_t pid2=0,pid3=0,pid4=0,pid5=0,pid6=0,pid7=0;
     init();
     info(BEGIN,1,0);
     pid2=fork();
@@ -34,6 +56,15 @@ int main(){
                     return 1;
                 }else if(pid7 ==0){
                     info(BEGIN,7,0);
+                    TH_STRUCT params[3];
+                    pthread_t tids[3];
+                    for(int i=0;i<3;i++){
+                        params[i].value = i+2;
+                        pthread_create(&tids[i],NULL,thread_function,&params[i]);
+                    }
+                    for(int i=0;i<3;i++){
+                        pthread_join(tids[i],NULL);
+                    }
                     info(END,7,0);
                 }else{
                     wait(NULL);
@@ -52,6 +83,10 @@ int main(){
                 return 1;
             }else if(pid5 == 0){
                 info(BEGIN,5,0);
+                int nrThreads= 42;
+                pthread_t tids[42];
+                sem_t mutex;
+                sem_t barrier;
                 pid6= fork();
                 if(pid6 == -1){
                     perror("could not create p6");
