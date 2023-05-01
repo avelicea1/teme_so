d735c09bcb6432ac2aa6c8bdf5e319512dd4b330
Andreea <avelicea1@yahoo.com>
2023-05-01 16:14:29 +0300

    nu mai pot

--- a2/a2.c
+++ a2/a2.c
@@ -5,32 +5,40 @@
 #include "a2_helper.h"
 #include <pthread.h>
 #include <semaphore.h>
-typedef struct{
+typedef struct
+{
     int value;
-}TH_STRUCT;
-typedef struct{
+} TH_STRUCT;
+typedef struct
+{
     int id;
     sem_t *log;
     pthread_mutex_t *mutex;
     pthread_cond_t *cond_13;
     pthread_cond_t *cond;
-}TH_STRUCT_P5;
+    pthread_mutex_t *barrier;
+    int nr_still_to_come;
+} TH_STRUCT_P5;
 
-void *thread_function(void *args){
-    TH_STRUCT *s = (TH_STRUCT*)args;
-    info(BEGIN,7,s->value);
-    if(s->value ==4){ 
+void *thread_function(void *args)
+{
+    TH_STRUCT *s = (TH_STRUCT *)args;
+    info(BEGIN, 7, s->value);
+    if (s->value == 4)
+    {
         pthread_t tid1;
-        int i=1;
-        pthread_create(&tid1,NULL,thread_function,&i);
-        pthread_join(tid1,NULL);
+        int i = 1;
+        pthread_create(&tid1, NULL, thread_function, &i);
+        pthread_join(tid1, NULL);
     }
-    info(END,7,s->value);
+    info(END, 7, s->value);
     return NULL;
 }
 int count = 0;
-int s_13 =0;
-void *thread_function_p5(void*args){
+int s_13 = 0;
+int v[6]={0};
+void *thread_function_p5(void *args)
+{
     TH_STRUCT_P5 *s = (TH_STRUCT_P5*)args;
     sem_wait(s->log);
     info(BEGIN,5,s->id);
@@ -57,106 +65,141 @@ void *thread_function_p5(void*args){
     sem_post(s->log);
     return NULL;
 }
-int main(){
-    pid_t pid2=0,pid3=0,pid4=0,pid5=0,pid6=0,pid7=0;
+int main()
+{
+    pid_t pid2 = 0, pid3 = 0, pid4 = 0, pid5 = 0, pid6 = 0, pid7 = 0;
     init();
-    info(BEGIN,1,0);
-    pid2=fork();
-    if(pid2==-1){
+    info(BEGIN, 1, 0);
+    pid2 = fork();
+    if (pid2 == -1)
+    {
         perror("could not create p2");
         return 1;
-    }else if(pid2==0){
-        info(BEGIN,2,0);
-        info(END,2,0);
-    }else{
+    }
+    else if (pid2 == 0)
+    {
+        info(BEGIN, 2, 0);
+        info(END, 2, 0);
+    }
+    else
+    {
         wait(NULL);
         pid3 = fork();
-        if(pid3 == -1){
+        if (pid3 == -1)
+        {
             perror("could not create p3");
             return 1;
-        }else if(pid3 == 0){
-            info(BEGIN,3,0);
-            pid4=fork();
-            if(pid4 == -1){
+        }
+        else if (pid3 == 0)
+        {
+            info(BEGIN, 3, 0);
+            pid4 = fork();
+            if (pid4 == -1)
+            {
                 perror("could not create p4");
                 return 1;
-            }else if(pid4 == 0){
-                info(BEGIN,4,0);
+            }
+            else if (pid4 == 0)
+            {
+                info(BEGIN, 4, 0);
                 pid7 = fork();
-                if(pid7 == -1){
+                if (pid7 == -1)
+                {
                     perror("could not create p7");
                     return 1;
-                }else if(pid7 ==0){
-                    info(BEGIN,7,0);
+                }
+                else if (pid7 == 0)
+                {
+                    info(BEGIN, 7, 0);
                     TH_STRUCT params[3];
                     pthread_t tids[3];
-                    for(int i=0;i<3;i++){
-                        params[i].value = i+2;
-                        pthread_create(&tids[i],NULL,thread_function,&params[i]);
+                    for (int i = 0; i < 3; i++)
+                    {
+                        params[i].value = i + 2;
+                        pthread_create(&tids[i], NULL, thread_function, &params[i]);
                     }
-                    for(int i=0;i<3;i++){
-                        pthread_join(tids[i],NULL);
+                    for (int i = 0; i < 3; i++)
+                    {
+                        pthread_join(tids[i], NULL);
                     }
-                    info(END,7,0);
-                }else{
-                    wait(NULL);
-                    info(END,4,0);
+                    info(END, 7, 0);
                 }
-                
-            }else{
+                else
+                {
+                    wait(NULL);
+                    info(END, 4, 0);
+                }
+            }
+            else
+            {
                 wait(NULL);
-                info(END,3,0);
-            } 
-        }else{
+                info(END, 3, 0);
+            }
+        }
+        else
+        {
             wait(NULL);
             pid5 = fork();
-            if(pid5 == -1){
+            if (pid5 == -1)
+            {
                 perror("could not create p5");
                 return 1;
-            }else if(pid5 == 0){
-                info(BEGIN,5,0);
-                int nrThreads= 42;
+            }
+            else if (pid5 == 0)
+            {
+                info(BEGIN, 5, 0);
+                int nrThreads = 42;
                 pthread_t tids[nrThreads];
                 TH_STRUCT_P5 params[nrThreads];
                 sem_t log;
                 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
                 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
                 pthread_cond_t cond_13 = PTHREAD_COND_INITIALIZER;
-                sem_init(&log,0,6);
-                for(int i=0;i<nrThreads;i++){
-                    params[i].id = i+1;
+                pthread_mutex_t barrier = PTHREAD_MUTEX_INITIALIZER;
+                sem_init(&log, 0, 6);
+                for (int i = 0; i < nrThreads; i++)
+                {
+                    params[i].id = i + 1;
                     params[i].log = &log;
                     params[i].mutex = &mutex;
                     params[i].cond = &cond;
                     params[i].cond_13 = &cond_13;
-                    pthread_create(&tids[i],NULL,thread_function_p5,&params[i]);
+                    params[i].barrier = &barrier;
+                    pthread_create(&tids[i], NULL, thread_function_p5, &params[i]);
                 }
-                for(int i=0;i<nrThreads;i++){
-                    pthread_join(tids[i],NULL);
+                for (int i = 0; i < nrThreads; i++)
+                {
+                    pthread_join(tids[i], NULL);
                 }
                 sem_destroy(&log);
                 pthread_mutex_destroy(&mutex);
                 pthread_cond_destroy(&cond);
                 pthread_cond_destroy(&cond_13);
-                pid6= fork();
-                if(pid6 == -1){
+                pthread_mutex_destroy(&barrier);
+                pid6 = fork();
+                if (pid6 == -1)
+                {
                     perror("could not create p6");
                     return 1;
-                }else if(pid6 == 0){
-                    info(BEGIN,6,0);
-                    info(END,6,0);
-                }else{
-                    wait(NULL);
-                    info(END,5,0);
                 }
-            }else{
-                wait(NULL);
-                wait(NULL);
-                info(END,1,0);
+                else if (pid6 == 0)
+                {
+                    info(BEGIN, 6, 0);
+                    info(END, 6, 0);
+                }
+                else
+                {
+                    wait(NULL);
+                    info(END, 5, 0);
+                }
             }
-            
+            else
+            {
+                wait(NULL);
+                wait(NULL);
+                info(END, 1, 0);
+            }
         }
-        
     }
     return 0;
 }
